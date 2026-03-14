2e47a2ddf2f72f2a8744c3dd5e97263a32956b17
pwkkos <jasmineluc90@gmail.com>
2026-03-13 21:11:33 -0700

    update game

--- godot-minigame-3/scripts/toy.gd
+++ godot-minigame-3/scripts/toy.gd
@@ -17,22 +17,30 @@ func _process(_delta: float) -> void:
 	var y = global_transform.origin.y
 	
 	# minigame step 7
-	# if x > 0 && y > 0:
-	# 	%Tilemaps._highlightLowerRight()
+	if x > 0 && y > 0:
+		%Tilemaps._highlightLowerRight()
+	elif x < 0 && y < 0:
+		%Tilemaps._highlightUpperRight()
+	elif x < 0 && y > 0:
+		%Tilemaps._highlightLowerLeft()
+	elif x < 0 && y < 0:
+		%Tilemaps._highlightUpperLeft()
 	
 	
 
 func _on_body_entered(_body: Node) -> void:
 	# minigame step 2
 	_bounces += 1
-	# %UI._update_Bounces(_bounces)
+	%UI._update_Bounces(_bounces)
+	
 	
 	# minigame step 4 (uncomment)
-	# var c = _create_Color(_bounces)
-	# _sprite.modulate = c
+	var c = _create_Color(_bounces)
+	_sprite.modulate = c
+	
 	
 	# minigame step 6 (uncomment)
-	# %UI._update_Color(c.r, c.g, c.b)
+	%UI._update_Color(c.r, c.g, c.b)
 		
 func _create_Color(bounces: int) -> Color:
 	var r = 0.0
@@ -40,9 +48,26 @@ func _create_Color(bounces: int) -> Color:
 	var b = 0.0
 	
 	# minigame step 3
-	# write your if statments below this comment
-	
-		
+	if bounces == 0:
+		r = 5.0
+		g = 1.0
+		b = 1.0
+	elif bounces < 10:
+		r = 0.0
+		g = 1.0
+		b = 5.0
+	elif bounces <= 20:
+		r = 5.0
+		g = 0.0
+		b = 5.0
+	elif bounces != 20 && bounces < 30:
+		r = 2.0
+		g = 8.0
+		b = 0.0
+	else:
+		r = 0.0
+		g = 3.0
+		b = 3.0
 	return Color(r, g, b)
 	
 # playground 5
@@ -54,11 +79,71 @@ func _add(a: int, b: int) -> int:
 func _playground() -> void:
 	# playground 1
 	var message = "Hello world!"
+	print(message)
+
+# playground 2
+	var seconds = 12.5
+	message = "seconds = %f" % seconds
+	print(message)
+
+# playground 3
+	message = "seconds = %.1f" % seconds
+	print(message)
+
+# playground 4
+	var x = 1.0
+	var y = 3.4
+	var z = 20.5
+	message = "position = (%.1f, %.1f, %.1f)" % [x, y, z]
+	print(message)
 	
+	# playground 6
+	var v1 = 4
+	var v2 = 3
+	var sum_1_2 = _add(v1, v2)
+	print("sum: %s" % sum_1_2)
+	
+# playground 7
+	sum_1_2 = _add(10, 2)
+	print("second sum: %s" % sum_1_2)
+	
+	# playground 9
+	_print_name("Jasmine")
+	
+	# playground 10
+	var foo = true
+	print(foo)
 
+# playground 12
+	x = -1.0
+	y = 19.1
+# playground 13
+	var xLarger = _checkIfXIsLarger(x, y)
+	print("is x larger? %s" % xLarger)
+	x = 2
+	y = 3
+	var comparison = x >= y
+	print("comparison result: %s" % comparison)
+	# playground 14
+	x = 4
+	y = 3
+	var w = 10
+	var u = 0
+	comparison = x > y && w < u
+	print("comparison result 2: %s" % comparison)
+# playground 15
+	x = 6
+	if x < 5:
+		print("x is small")
+	elif x < 10:
+		print("x is medium")
+	else:
+		print("x is big")
+		
 # playground 8
-# put ya func here
-
+func _print_name(n) -> void:
+	print("your name is: %s" % n)
 
 # playground 11
-# put ya func here
+func _checkIfXIsLarger(x: float, y: float) -> bool:
+	return x > y
--- godot-minigame-3/scripts/ui.gd
+++ godot-minigame-3/scripts/ui.gd
@@ -4,12 +4,12 @@ extends HBoxContainer
 @onready var _colorText = get_node("ColorLabel")
 
 # minigame step 1
-# func _update_Bounces??:
-#	var text = ??
-#	_bounceText.text = text
+func _update_Bounces(b):
+	var text = "bounces %d" % b
+	_bounceText.text = text
 	
 
 # minigame step 5
-# func _update_Color???
-	# var colorText = ???
-	# _colorText.text = colorText
+func _update_Color(r: float, g: float, b: float) -> void:
+	var colorText = "color (%.1f, %.1f, %.1f)" % [r,g,b]
+	_colorText.text = colorText
